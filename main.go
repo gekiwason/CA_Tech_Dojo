7c6464dd549616df8b8b9500551666cc1ff569f4
wason <ninjinx8888@gmail.com>
2021-02-13 23:21:05 +0900

    formatかけてみた

--- main.go
+++ main.go
@@ -1,182 +1,214 @@
 package main
- 
-import (
-    "fmt"
-    // "log"
-    "net/http"
-    "crypto/md5"
-    "io"
-    "math/rand"
-    "time"
 
-    "github.com/gin-gonic/gin" 
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func gormConnect() *gorm.DB {
-    DBMS := "mysql"
-    USER := "root"
-    PASS := "root"
-    PROTOCOL := "tcp(localhost:3306)"
-    DBNAME := "CA_Tech_Dojo"
-    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
-    db, err := gorm.Open(DBMS, CONNECT)
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "root"
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "CA_Tech_Dojo"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
 
-    if err != nil {
-        panic(err.Error())
-    }
-    fmt.Println("db connected: ", &db)
-    return db
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("db connected: ", &db)
+	return db
 }
 
 type User struct {
-    ID       uint   `gorm:"primaryKey"`
-    Name  string `json:name gorm:"unique"`
-    Token  string `json:token`
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `json:name gorm:"unique"`
+	Token string `json:token`
 }
 
 type Character struct {
-    ID       uint       `gorm:"primaryKey"`
-    Name  string
-    Rarity  uint8
+	ID     uint `gorm:"primaryKey"`
+	Name   string
+	Rarity uint8
 }
 
 type UserCharacter struct {
-    ID                   uint   `gorm:"primaryKey"`
-    UserID            uint
-    CharacterID     uint
+	ID          uint `gorm:"primaryKey"`
+	UserID      uint
+	CharacterID uint
 }
 
 type GachaDrawRequest struct {
-    Times   int
+	Times int
 }
 
 func main() {
-    r := gin.Default()
-    r.POST("/user/create", create)
-    r.OPTIONS("/user/create", Options)
-    r.GET("/user/get", get)
-    r.PUT("/user/update", put)
-    r.POST("/gacha/draw", gacha)
-    r.Run(":8000")
+	r := gin.Default()
+	r.POST("/user/create", create)
+	r.OPTIONS("/user/create", Options)
+	r.GET("/user/get", get)
+	r.PUT("/user/update", put)
+	r.POST("/gacha/draw", gacha)
+	r.GET("/character/list", list)
+	r.Run(":8000")
+}
+
+func list(c *gin.Context) {
+	db := gormConnect()
+	db.LogMode(true)
+	user := User{}
+	userCharacters := []UserCharacter{}
+	// characters := []Character{}
+
+	token := c.Request.Header.Get("x-token")
+	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
+		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+		return
+	}
+
+	if err := db.Where("user_id = ?", user.ID).Find(&userCharacters).Error; err != nil {
+		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+		return
+	}
+
+	// for k, v := range userCharacters {
+	//     if err := db.Where("id = ?", userCharacters.CharacterID).Find(&characters).Error; err != nil {
+	//         c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+	//         return
+	//     }
+	// }
+
+	log.Printf("%v %#v %T\n", userCharacters, userCharacters, userCharacters)
 }
 
 func gacha(c *gin.Context) {
-    db := gormConnect()
-    db.LogMode(true)
-    db.Set("gorm:table_options", "ENGINE=InnoDB")
-    db.AutoMigrate(&Character{})
-    db.AutoMigrate(&UserCharacter{})
+	db := gormConnect()
+	db.LogMode(true)
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+	db.AutoMigrate(&Character{})
+	db.AutoMigrate(&UserCharacter{})
 
-    user := User{}
-    token := c.Request.Header.Get("x-token")
-    db.Where("token = ?", token).First(&user)
+	user := User{}
+	token := c.Request.Header.Get("x-token")
+	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
+		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+		return
+	}
 
-    raritys := map[int]int{
-        3: 300,
-        2: 1200,
-        1: 8500,
-    }
+	raritys := map[int]int{
+		3: 300,
+		2: 1200,
+		1: 8500,
+	}
 
-    req := GachaDrawRequest{}
-    c.BindJSON(&req)
-    times := req.Times
+	req := GachaDrawRequest{}
+	c.BindJSON(&req)
+	times := req.Times
 
-    result := Character{}
-    characters := []Character{}
-    for i := 1; i <= times; i++ {
+	result := Character{}
+	characters := []Character{}
+	for i := 1; i <= times; i++ {
 
-        userCharacter := UserCharacter{}
-        userCharacter.UserID = user.ID
+		userCharacter := UserCharacter{}
+		userCharacter.UserID = user.ID
 
-        rand.Seed(time.Now().UnixNano())
-        random := (rand.Intn(10000))
-        probability := 0
+		rand.Seed(time.Now().UnixNano())
+		random := (rand.Intn(10000))
+		probability := 0
 
-        for rarity, rarity_probability := range raritys {
-            probability += rarity_probability
-            if random <= probability { // 排出レアリティ確定
-                db.Where("rarity = ?", rarity).Find(&characters) // 排出レアリティ内からランダムに1枚取得
-                num := rand.Intn(len(characters))
-                result = characters[num]
+		for rarity, rarity_probability := range raritys {
+			probability += rarity_probability
+			if random <= probability { // 排出レアリティ確定
+				db.Where("rarity = ?", rarity).Find(&characters) // 排出レアリティ内のキャラ一覧を取得
+				num := rand.Intn(len(characters))
+				result = characters[num]
 
-                userCharacter.CharacterID = result.ID
-                db.Create(&userCharacter)
+				userCharacter.CharacterID = result.ID
+				db.Create(&userCharacter)
 
-                break
-            }
-        }
+				break
+			}
+		}
 
-        time.Sleep(1 * time.Second) // seed値が近すぎるとガチャ排出
-    }
+		time.Sleep(1 * time.Second) // seed値をずらしガチャ排出を均一に
+	}
 
-    c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, result)
 }
 
 func Options(c *gin.Context) {
-    if c.Request.Method != "OPTIONS" {
-        c.Next()
-    } else {
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
-        c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
-        c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
-        c.Header("Content-Type", "application/json")
-        c.AbortWithStatus(http.StatusOK)
-    }
+	if c.Request.Method != "OPTIONS" {
+		c.Next()
+	} else {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
+		c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Content-Type", "application/json")
+		c.AbortWithStatus(http.StatusOK)
+	}
 }
 
 func create(c *gin.Context) {
-    db := gormConnect()
-    db.LogMode(true)
-    db.Set("gorm:table_options", "ENGINE=InnoDB")
-    db.AutoMigrate(&User{})
+	db := gormConnect()
+	db.LogMode(true)
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+	db.AutoMigrate(&User{})
 
-    user := User{}
-    c.BindJSON(&user)
+	user := User{}
+	c.BindJSON(&user)
 
-    token := "q3489fj9q0348nfq034mf"
-    token = user.Name + token
-    h := md5.New()
-    io.WriteString(h, token)
-    s := fmt.Sprintf("%x", h.Sum(nil))
+	token := "q3489fj9q0348nfq034mf"
+	token = user.Name + token
+	h := md5.New()
+	io.WriteString(h, token)
+	s := fmt.Sprintf("%x", h.Sum(nil))
 
-    user.Token = s
+	user.Token = s
 
-    db.NewRecord(user)
-    db.Create(&user)
-    if db.NewRecord(user) == false {
-        c.JSON(http.StatusOK, gin.H{
-            "token": user.Token,
-        })
-    }
+	db.NewRecord(user)
+	db.Create(&user)
+	if db.NewRecord(user) == false {
+		c.JSON(http.StatusOK, gin.H{
+			"token": user.Token,
+		})
+	}
 }
 
 func get(c *gin.Context) {
-    db := gormConnect()
-    db.LogMode(true)
+	db := gormConnect()
+	db.LogMode(true)
 
-    user := User{}
-    token := c.Request.Header.Get("x-token")
+	user := User{}
+	token := c.Request.Header.Get("x-token")
 
-    db.Where("token = ?", token).First(&user)
-    c.JSON(http.StatusOK, gin.H{
-        "name": user.Name,
-    })
+	db.Where("token = ?", token).First(&user)
+	c.JSON(http.StatusOK, gin.H{
+		"name": user.Name,
+	})
 }
 
 func put(c *gin.Context) {
-    db := gormConnect()
-    db.LogMode(true)
+	db := gormConnect()
+	db.LogMode(true)
 
-    user := User{}
-    token := c.Request.Header.Get("x-token")
+	user := User{}
+	token := c.Request.Header.Get("x-token")
 
-    data := User{}
-    if err := c.BindJSON(&data); err != nil {
-      c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
-    }
+	data := User{}
+	if err := c.BindJSON(&data); err != nil {
+		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+	}
 
-    db.Where("token = ?", token).First(&user).Updates(&data)
+	db.Where("token = ?", token).First(&user).Updates(&data)
 }
