1fc6ac7853be817fbd2892e3b7c4cc497bade12b
wason <ninjinx8888@gmail.com>
2021-02-14 19:19:07 +0900

    keep neat and tidy

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -81,105 +80,6 @@ func main() {
 	r.Run(":8000")
 }
 
-func list(c *gin.Context) {
-	db := gormConnect()
-	user := User{}
-	userCharacters := []UserCharacter{}
-
-	token := c.Request.Header.Get("x-token")
-	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
-		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
-		return
-	}
-
-	if err := db.Where("user_id = ?", user.ID).Find(&userCharacters).Error; err != nil {
-		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
-		return
-	}
-
-	res := []CharacterListResponse{}
-	for _, v := range userCharacters {
-		character := Character{}
-		characterForRes := CharacterListResponse{}
-		log.Println(v.CharacterID)
-		if err := db.First(&character, "id = ?", v.CharacterID).Error; err != nil {
-			c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
-			return
-		}
-
-		characterForRes.UserCharacterID = fmt.Sprint(v.ID)
-		characterForRes.CharacterID = fmt.Sprint(character.ID)
-		characterForRes.Name = character.Name
-
-		res = append(res, characterForRes)
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"characters": res,
-	})
-}
-
-func gacha(c *gin.Context) {
-	db := gormConnect()
-
-	user := User{}
-	token := c.Request.Header.Get("x-token")
-	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
-		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
-		return
-	}
-
-	raritys := map[int]int{
-		3: 300,
-		2: 1200,
-		1: 8500,
-	}
-
-	req := GachaDrawRequest{}
-	c.BindJSON(&req)
-	times := req.Times
-
-	result := Character{}
-	characters := []Character{}
-	results := []GachaResult{}
-
-	for i := 1; i <= times; i++ {
-
-		userCharacter := UserCharacter{}
-		userCharacter.UserID = user.ID
-
-		rand.Seed(time.Now().UnixNano())
-		random := (rand.Intn(10000))
-		probability := 0
-
-		gachaResult := GachaResult{}
-		for rarity, rarity_probability := range raritys {
-			probability += rarity_probability
-			if random <= probability { // 排出レアリティ確定
-				db.Where("rarity = ?", rarity).Find(&characters) // 排出レアリティ内のキャラ一覧を取得
-				num := rand.Intn(len(characters))
-				result = characters[num]
-
-				userCharacter.CharacterID = result.ID
-				db.Create(&userCharacter)
-
-				gachaResult.CharacterID = fmt.Sprint(result.ID)
-				gachaResult.Name = result.Name
-				results = append(results, gachaResult)
-
-				log.Printf("%v", results)
-				break
-			}
-		}
-
-		time.Sleep(1 * time.Second) // seed値をずらしガチャ排出を均一に
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"results": results,
-	})
-}
-
 func create(c *gin.Context) {
 	db := gormConnect()
 
@@ -253,4 +153,109 @@ func put(c *gin.Context) {
 	}
 
 	c.Status(http.StatusOK)
+}
+
+func gacha(c *gin.Context) {
+	db := gormConnect()
+
+	user := User{}
+	token := c.Request.Header.Get("x-token")
+	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	raritys := map[int]int{
+		3: 300,  //SSR
+		2: 1200, //SR
+		1: 8500, //R
+	}
+
+	req := GachaDrawRequest{}
+	c.BindJSON(&req)
+	times := req.Times
+
+	result := Character{}
+	characters := []Character{}
+	gachaResults := []GachaResult{}
+
+	for i := 1; i <= times; i++ {
+
+		userCharacter := UserCharacter{}
+		userCharacter.UserID = user.ID
+
+		rand.Seed(time.Now().UnixNano())
+		random := (rand.Intn(10000))
+		probability := 0
+
+		gachaResult := GachaResult{}
+		for rarity, rarityProbability := range raritys {
+			probability += rarityProbability
+			if random <= probability { // 排出レアリティ確定
+				db.Where("rarity = ?", rarity).Find(&characters) // 排出レアリティ内のキャラ一覧を取得
+				num := rand.Intn(len(characters))
+				result = characters[num]
+
+				userCharacter.CharacterID = result.ID
+				db.Create(&userCharacter)
+
+				gachaResult.CharacterID = fmt.Sprint(result.ID)
+				gachaResult.Name = result.Name
+				gachaResults = append(gachaResults, gachaResult)
+
+				break
+			}
+		}
+
+		time.Sleep(1 * time.Second) // seed値をずらしガチャ排出を均一に
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"results": gachaResults,
+	})
+}
+
+func list(c *gin.Context) {
+	db := gormConnect()
+	user := User{}
+	userCharacters := []UserCharacter{}
+
+	token := c.Request.Header.Get("x-token")
+	if err := db.Where("token = ?", token).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := db.Where("user_id = ?", user.ID).Find(&userCharacters).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	res := []CharacterListResponse{}
+	for _, v := range userCharacters {
+		character := Character{}
+		characterForRes := CharacterListResponse{}
+		if err := db.First(&character, "id = ?", v.CharacterID).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		characterForRes.UserCharacterID = fmt.Sprint(v.ID)
+		characterForRes.CharacterID = fmt.Sprint(character.ID)
+		characterForRes.Name = character.Name
+
+		res = append(res, characterForRes)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"characters": res,
+	})
 }
